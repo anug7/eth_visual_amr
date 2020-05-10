9b38397c08003fe7a9b0e23d0ad0bfc2b2f2b097
guna007 <gunasekaran.s@vimaanrobotics.com>
2020-05-10 20:51:59 +0530

    Assign 3 till description creation

--- assigns/exer03_keypoint_tracker/code/main.py
+++ assigns/exer03_keypoint_tracker/code/main.py
@@ -1,6 +1,6 @@
-
-
 import numpy as np
+
+import cv2
 
 
 def apply_conv(array, filter_array):
@@ -10,7 +10,7 @@ def apply_conv(array, filter_array):
     @param: filter_array: conv filter array
     """
     pad_size = filter_array.shape[0] - 2
-    pad_array = np.pad(array, (1,), "constant", constant_values=(0))
+    pad_array = np.pad(array, (pad_size,), "constant", constant_values=(0))
     sub_mat_size = filter_array.shape
 
     sub_matrices_shape = tuple(np.subtract(array.shape, sub_mat_size) + 1) + sub_mat_size
@@ -26,10 +26,20 @@ def apply_conv(array, filter_array):
 def calc_cornerness(Ix, Iy, k=0.04):
     """
     """
-    Ix2 = (Ix**2).astype('float')
-    Iy2 = (Iy**2).astype('float')
-    Ixy2 = (Ix * Iy)**2
-    first, second = Ix2 + Iy2, np.sqrt(4*Ixy2 + (Ix2-Iy2)**2)
+    kern = np.asarray([[0.0625, 0.125 , 0.0625],
+                       [0.125 , 0.25  , 0.125],
+                       [0.0625, 0.125 , 0.0625]])
+    Ix2 = (Ix**2)
+    Iy2 = (Iy**2)
+    
+    sIx2 = apply_conv(Ix2, kern)
+    sIy2 = apply_conv(Iy2, kern)
+    
+    Ixy = (Ix * Iy)
+    sIxy = apply_conv(Ixy, kern)
+    sIxy2 = sIxy ** 2 
+    
+    first, second = sIx2 + sIy2, np.sqrt(4*sIxy2 + (sIx2-sIy2)**2)
     l1 = 0.5 * (first + second)
     l2 = 0.5 * (first - second)
     
@@ -42,3 +52,30 @@ def calc_harris_scores(l1, l2,k=0.04):
 
 def calc_tomashi_score(l1, l2):
     return np.min(l1, l2)
+
+
+def non_maxima_suppression(scores, no_of_points=40, w=3):
+    locs = []
+    for i in range(40):
+        x, y = np.where(scores == np.amax(scores))
+        scores[x[0] - w: x[0] + w, y[0] - w: y[0] + w] = np.zeros((w*2, w*2))
+        locs.append((x[0], y[0]))
+    return locs
+
+
+def create_keypoint_desp(img, locs, d=144):
+    """
+    Create keypoint description based in image intensity values
+    """
+    desp, w = [], int(np.sqrt(d) / 2.)
+    for l in locs:
+        op = img[l[0] - w: l[0] + w, l[1] - w: l[1] + w].flatten()
+        op = np.pad(op, (d - op.shape[0],), "constant", constant_values=(0))
+        desp.append(op)
+    return np.asarray(desp).T
+
+
+def draw_keypoints(img, points, color=(0, 0, 255)):
+    for p in points:
+        img = cv2.circle(img,  (p[0], p[1]), 2, color, 2)
+    return img
