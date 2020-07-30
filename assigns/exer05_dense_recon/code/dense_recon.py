7c1f9a8faf3afe04338c4e3c7a4407be20e7391e
guna007 <gunasekaran@solitontech.com>
2020-07-30 10:25:12 +0530

    Heapify to outlier removeal in ssd matching

--- assigns/exer05_dense_recon/code/dense_recon.py
+++ assigns/exer05_dense_recon/code/dense_recon.py
@@ -1,4 +1,4 @@
-
+from heapq import heapify, heappush
 import numpy as np
 import cv2
 
@@ -7,23 +7,34 @@ limg = cv2.imread("../data/left/000000.png", 0)
 rimg = cv2.imread("../data/right/000000.png", 0)
 
 # Given by the KITTI dataset:
-baseline = 0.54;
+baseline = 0.54
 
 # Carefully tuned by the TAs:
 patch_radius = 5
-min_disp = 5
-max_disp = 50
+min_disp, max_disp = 5, 50
 disparity = np.zeros_like(limg)
 for x in range(patch_radius + max_disp + 1, limg.shape[1] - patch_radius - 1):
     for y in range(patch_radius + 1, limg.shape[0] - patch_radius - 1):
         lblock = limg[y - patch_radius - 1: y + patch_radius,
                       x - patch_radius - 1: x + patch_radius]
-        val_idx, min_val = 0, 10000000
-        for d in range(5, 50):
+        min_val, values, val_idx = 10000000, [], 0
+        heapify(values)
+        for d in range(min_disp, max_disp + 1):
             rblock = rimg[y - patch_radius - 1: y + patch_radius,
                           x - patch_radius - 1 - d: x + patch_radius - d]
             val = np.sum((rblock - lblock)**2)
+            heappush(values, val)
             if val < min_val:
                 val_idx = d
                 min_val = val
-        disparity[y, x] = val_idx
+        deno = float(values[0]) if values[0] > 0 else 200.0
+        fact = np.asarray(values[1:4]) / deno
+        if fact[0] < 1.5 and fact[1] < 1.5 and fact[2] < 1.5:
+            disparity[y, x] = 0
+        else:
+            if val_idx != min_disp  and val_idx != max_disp:
+                disparity[y, x] = val_idx
+
+op_img = cv2.applyColorMap(disparity * 5, cv2.COLORMAP_JET)
+cv2.imshow("op", op_img)
+cv2.waitKey(0)
