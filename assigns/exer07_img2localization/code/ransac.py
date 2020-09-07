c1e41112b545b70a8378ecacffce39c6855994b4
guna007 <gunasekaran.s@vimaanrobotics.com>
2020-09-07 16:35:28 +0530

    Just RANSAC implementation

--- assigns/exer07_img2localization/code/ransac.py
+++ assigns/exer07_img2localization/code/ransac.py
@@ -11,20 +11,29 @@ def ransac_polynomial(X, Y, max_noise, no_of_points=3, iters=200):
   RANSAC for polynomial fit with know max noise in data
   """
   end, cur_guess_idx, cur_guess_cnt = X.shape[0], 0, 0
-  history_inliers = []
+  hist_inliers = []
   for i in range(iters):
-    idcs = np.random.randint(0, end, size=no_of_points)
-    sampx, sampy = X[idcs], Y[idcs]
+    full_idcs = np.arange(end)
+    np.random.shuffle(full_idcs)
+    idcs1 = full_idcs[:no_of_points]
+    idcs2 = full_idcs[no_of_points:]
+    sampx = X[idcs1]
+    sampy = Y[idcs1]
+    testx = X[idcs2]
+    testy = Y[idcs2]
     coefs = np.polyfit(sampx, sampy, 2)
-    fwd = np.polyval(coefs, X)
-    vals = abs(fwd - Y)
-    no_of_inliers = np.sum(vals <= max_noise)
+    fwd = np.polyval(coefs, testx)
+    vals = abs(fwd - testy)
+    inliers = vals <= max_noise
+    no_of_inliers = np.sum(inliers)
     if no_of_inliers >= cur_guess_cnt:
-      history_inliers.append({'coefs': coefs,
-          'inliers': [X[vals >= max_noise], Y[vals>=max_noise]]})
+      hist_inliers.append({'coefs': coefs,
+                              'inliers': [testx[inliers], testy[inliers]]})
       cur_guess_cnt = no_of_inliers
-      plt.scatter(X, fwd)
-  return history_inliers
+  inx = hist_inliers[-1]['inliers'][0]
+  iny = hist_inliers[-1]['inliers'][1]
+  ncoefs = np.polyfit(inx, iny, 2)
+  return ncoefs
 
 num_inliers = 20
 num_outliers = 10
@@ -47,4 +56,9 @@ y_outlier = rand(num_outliers) * yspan + lowest
 X = np.hstack((x, x_outlier))
 Y = np.hstack((y, y_outlier))
 
-hist_inliers = ransac_polynomial(X, Y, max_noise)
+plt.scatter(X, Y, marker="+")
+ncoefs = ransac_polynomial(X, Y, max_noise)
+plt.plot(np.sort(x), np.polyval(ncoefs, np.sort(x)))
+plt.show()
+
+print("op")
