3d4569e7123ab7b90ce1c72a11f9e9c9a40a77ac
guna007 <gunasekaran@solitontech.com>
2020-09-20 20:10:28 +0530

    KLT completed in matlab
    1. After a long struggle, found the interpolation was wrongly done
    2. Used some online reference to fix the issue
    
    Refs:
    1. https://dsp.stackexchange.com/questions/9604/jacobian-of-affine-warp-in-lucas-kanade-image-alignment
    2. https://www.ri.cmu.edu/pub_files/pub3/baker_simon_2004_1/baker_simon_2004_1.pdf
    3. https://www.crcv.ucf.edu/wp-content/uploads/2019/03/Lecture-10-KLT.pdf

--- assigns/exer08_lkt/code/getWarpedPatch.m
+++ assigns/exer08_lkt/code/getWarpedPatch.m
@@ -1,5 +1,40 @@
 function patch = getWarpedPatch(I, W, x_T, r_T)
+% patch = getWarpedPatch(I, W, x_T, r_T);
 % x_T is 1x2 and contains [x_T y_T] as defined in the statement. patch is
 % (2*r_T+1)x(2*r_T+1) and arranged consistently with the input image I.
-I_R = warpImageCenter(I, W, x_T);
-patch = I_R(x_T(2) - r_T: x_T(2) + r_T, x_T(1) - r_T: x_T(1) + r_T);
+% Input:
+%   I, image
+%   W, 2x3 affine transformation matrix
+%   x_T, 1x2 patch translation (x,y)
+%   r_T, scalar, patch radius
+% Output:
+%   patch, s_T x s_T patch, s_T = 2*r_T+1
+
+patch = zeros(2*r_T+1);
+max_coords = fliplr(size(I));
+WT = W';
+inter = true;
+for x = -r_T:r_T
+    for y = -r_T:r_T
+        op = x_T + [x y 1] * WT;
+        if all(op < max_coords & op > [1 1])
+            if inter
+                p0 = floor(op);
+                coefs = op - p0;
+                a = coefs(1);
+                b = coefs(2);            
+                pval = (1-b) * (...
+                    (1-a) * I(p0(2), p0(1)) +...
+                    a * I(p0(2), p0(1)+1))...
+                    + b * (...
+                    (1-a) * I(p0(2)+1, p0(1)) +...
+                    a * I(p0(2)+1, p0(1)+1));
+                patch(y + r_T + 1, x + r_T + 1) = ...
+                    pval;
+            else
+                patch(y + r_T + 1, x + r_T + 1) = ...
+                    I(int16(op(2)), int16(op(1)));
+            end
+        end
+    end
+end
--- assigns/exer08_lkt/code/warpImageCenter.m
+++ assigns/exer08_lkt/code/warpImageCenter.m
@@ -1,12 +1,18 @@
 function I = warpImageCenter(I_R, W, cen)
 
 I = zeros(size(I_R), 'uint8');
-[height, width] = size(I_R);
+I_R = padarray(I_R, [1 1], 'replicate', 'post');
+[height, width] = size(I);
+[hr, wr] = size(I_R);
 for h=1:height
     for w=1:width
-        op = int16(W * [w - cen(1); h - cen(2); 1]);
-        if op(1) + cen(1) <= width && op(1) + cen(1) >= 1 && op(2) + cen(2) <= height && op(2) + cen(2) >=1
-            I(h, w) = I_R(op(2) + cen(2), op(1) + cen(1));
+        op = (W * [w - cen(1); h - cen(2); 1]) + cen';
+        p1 = ceil(op);
+        coef = double(op - p1);
+        if p1(1) < wr && p1(1) >= 1 && p1(2) < hr && p1(2) >=1
+            matval = [I_R(p1(2), p1(1)) I_R(p1(2) + 1, p1(1)); I_R(p1(2), p1(1) + 1) I_R(p1(2) + 1, p1(1) + 1)];
+            I(h, w) = [1 - coef(1) coef(1)] * double(matval)  * [1 - coef(2); coef(2)];
+            %I(h, w) = I_R(op(2) + cen(2), op(1) + cen(1));
         end 
     end
 end
