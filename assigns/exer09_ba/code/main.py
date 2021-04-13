e34768c2690e3ba60441f646af0f2f13cafed817
guna007 <gunasekaran.s@vimaanrobotics.com>
2021-04-13 17:59:16 +0530

    Debugging..

--- assigns/exer09_ba/code/main.py
+++ assigns/exer09_ba/code/main.py
@@ -7,6 +7,8 @@ import scipy.optimize as optimize
 from scipy.sparse import lil_matrix
 import matplotlib.pyplot as plt
 
+import cv2
+
 import helpers as hp
 
 
@@ -43,115 +45,82 @@ def project_points(twist, points_3d, kmat):
   """
   Projects 3D points on to image plane
   """
-  hmat = hp.twist_to_homo_mat(twist[:6])[:3, :]
-  hpoints_3d = np.hstack((points_3d, np.ones((points_3d.shape[0], 1))))
-  intm = kmat.dot(hmat)
-  out = intm.dot(hpoints_3d.T).T
-  out = out / out[:, [2, 2, 2]]
-  return out
+  hmat = hp.twist_to_homo_mat(twist[:6])
+  hmat = np.linalg.inv(hmat)[:3, :]
+  points_3d = np.hstack((points_3d, np.ones((points_3d.shape[0], 1))))
+  intm = hmat.dot(points_3d.T).T
+  out = kmat.dot(intm.T).T
+  out /= out[:, [2, 2, 2]]
+  return out[:, :2]
 
-def run_ba(hstates, aobsers, kmat):
+
+def error_function(hstates, observations, kmat):
   """
-  Run BA on image points and actual positions in 3D
-  @param: hstates: image coordinates of points
-  @param: aobsers: actual 3D point in the world
-  @param: kmat: Intrinsics of the camera
   """
-  nframes, nlandmarks = int(aobsers[0]), int(aobsers[1])
-  offset, pidx, err = 2, 0, []
-  total_3d_indices, total_2d_points = [], []
+  err, sidx = [], 2
+  nframes = int(observations[0])
+  poses = hstates[: nframes * 6].reshape((-1, 6))
+  p3d_points = hstates[nframes * 6: ].reshape((-1, 3))
   for i in range(nframes):
-    cur_nlandmarks = int(aobsers[offset])
-    lpoints = aobsers[ offset + 1: offset + 1 + cur_nlandmarks * 2].reshape((-1,2))
-    # swap row, column to x, y
-    lpoints[:, [1, 0]] = lpoints[:, [0, 1]]
+    cur_obs = int(observations[sidx])
+    sidx += 1
+    p2d_points = observations[sidx: sidx + 2 * cur_obs].reshape((-1, 2))
+    p2d_points[:, [1, 0]] = p2d_points[:, [0, 1]]
 
-    offset += 1 + cur_nlandmarks * 2
-    indices = aobsers[offset: offset + cur_nlandmarks].astype('uint') - 1 # make indices 0 based instead of 1 in Matlab
+    sidx += 2 * cur_obs
 
-    total_3d_indices.append(indices)
-    total_2d_points.append(lpoints)
+    # - 1 for 0 based index
+    idcs = observations[sidx: sidx + cur_obs].astype('uint32') - 1
 
-    offset += cur_nlandmarks
+    re_points = project_points(poses[i], p3d_points[idcs], kmat)
+    cur_err = (re_points - p2d_points).flatten()
 
-  def peval_func(thstates, t3d_indices, t2d_points, kmat):
-    """
-    """
-    no_frames = len(t2d_points)
-    err, ttwists = [], np.asarray(thstates[:no_frames * 6]).reshape((-1, 6))
-    _points_3d = thstates[no_frames * 6: ].reshape((-1, 3))
+    sidx += cur_obs
+    err.extend(cur_err)
 
-    for twist, c3d_indices, c2d_points in zip(ttwists, t3d_indices, t2d_points):
-      pro_points = project_points(twist, _points_3d[c3d_indices], kmat)[:, :2]
-      cerr = (pro_points - c2d_points).flatten()
-      err.extend(cerr)
-    return err
+  return err
 
-  def calc_jacobian(thstates, t3d_indices):
-    """
-    Calculates jacobian
-    """
-    # Refer: https://www.telesens.co/2016/10/13/bundle-adjustment-part-1-jacobians/
-    nframes = len(t3d_indices)
-    ttl_obs = 0
-    for i in range(nframes):
-      ttl_obs +=  len(t3d_indices[i])
-    rows, sidx = ttl_obs * 2, 0
-    #jac = np.zeros((rows, thstates.shape[0])).astype('uint')
-    jac = lil_matrix((rows, thstates.shape[0]), dtype=int)
-    for i in range(nframes):
-      cur_idx, lidx = t3d_indices[i], np.arange(len(t3d_indices[i]))
-      for cidx in range(6):
-        jac[sidx + lidx * 2, i * 6 + cidx] = 1
-        jac[sidx + lidx * 2 + 1, i * 6 + cidx] = 1
 
-      for pidx in range(3):
-        jac[sidx + lidx * 2, (nframes * 6) + (cur_idx * 3) + pidx] = 1
-        jac[sidx + lidx * 2 + 1, (nframes * 6) + (cur_idx * 3) + pidx] = 1
-      sidx += len(cur_idx) * 2
+def calc_jacobian(hstates, observations):
+  """
+  Calculates jacobian
+  """
+  # Refer: https://www.telesens.co/2016/10/13/bundle-adjustment-part-1-jacobians/
+  nframes = int(observations[0])
+  cols = hstates.shape[0]
+  indcs, sidx, rows = [], 2, 0
+  for i in range(nframes):
+    cur_nlandmarks = int(observations[sidx])
+    rows += cur_nlandmarks
+    sidx += cur_nlandmarks * 2 + 1
 
-    return jac
+    #get index of observations wrt landmarks
+    cidcs = observations[sidx: sidx + cur_nlandmarks].astype('uint')
+    indcs.append(cidcs)
+    sidx += cur_nlandmarks
+  rows *= 2 # 2 per observation
+  jacob = lil_matrix((rows, cols), dtype=np.uint32)
+  # jacob = np.zeros((rows, cols), dtype=np.uint16)
 
-  jacob = calc_jacobian(hstates, total_3d_indices)
-  # peval_func(hstates, total_3d_indices, total_2d_points, kmat)
-  res = optimize.least_squares(peval_func, hstates, ftol=1e-4,
-                               x_scale='jac', method="trf",
-                               jac_sparsity=jacob, verbose=2, max_nfev=20,
-                               args=(total_3d_indices, total_2d_points, kmat))
-  return res.x
+  sidx = 0
+  for i in range(nframes):
+    # - 1 for 0 based inex
+    p3d_idcs, p2d_idcs = indcs[i] - 1, np.arange(len(indcs[i]))
 
-hidden_states = np.loadtxt("../data/hidden_state.txt")
-observations = np.loadtxt("../data/observations.txt")
-NUM_FRAMES = 150
-K = np.loadtxt("../data/K.txt")
-poses = np.loadtxt("../data/poses.txt")
-pp_G_C = poses[:, [3, 7, 11]]
+    for cidx in range(6):
+      jacob[p2d_idcs * 2 + sidx, i*6 + cidx] = 1
+      jacob[p2d_idcs * 2 + 1 + sidx, i*6 + cidx] = 1
 
-hidden_states, observations, pp_G_C = hp.crop_problem(hidden_states,
-                                                      observations,
-                                                      pp_G_C,
-                                                      NUM_FRAMES)
-cropped_hidden_states, cropped_observations, _ = hp.crop_problem(hidden_states,
-                                                                 observations,
-                                                                 pp_G_C, 4)
+    for pidx in range(3):
+      jacob[p2d_idcs * 2 + sidx, nframes*6 + p3d_idcs*3 + pidx] = 1
+      jacob[p2d_idcs * 2 + 1 + sidx, nframes*6 + p3d_idcs*3 + pidx] = 1
 
-T_V_C = hidden_states[: NUM_FRAMES * 6].reshape(-1, 6)
-p_V_C = np.zeros((NUM_FRAMES, 3))
+    sidx += 2 * len(indcs[i])
 
-for i in range(NUM_FRAMES):
-  single_T_V_C = hp.twist_to_homo_mat(T_V_C[i])
-  p_V_C[i, :] = single_T_V_C[:3, 3]
+  return jacob
 
-p_G_C = align_estimate_to_gt(pp_G_C, p_V_C)
 
-#plt.plot(pp_G_C[:, 2], -pp_G_C[:, 0], color='g', label='Ground Truth')
-#plt.plot(p_V_C[:, 2], -p_V_C[:, 0], color='r', label='Original estimate')
-#plt.plot(p_G_C[:, 2], -p_G_C[:, 0], color='b', label='Aligned estimate')
-
-#plt.legend()
-#plt.show()
-
-def plot_map(_plt, hhidden_states, hobservations):
+def plot_map(_plt, hhidden_states, hobservations, pose_only=False):
   """
   """
   nnos = int(hobservations[0])
@@ -161,33 +130,82 @@ def plot_map(_plt, hhidden_states, hobservations):
   for n in range(nnos):
     T_W_frame = hp.twist_to_homo_mat(T_W_frames[n])
     p_W_frames[n, :] = T_W_frame[:3, 3]
-  _plt.scatter(p_W_landmarks[:, 2], -p_W_landmarks[:, 0])
+  if not pose_only:
+    _plt.scatter(p_W_landmarks[:, 2], -p_W_landmarks[:, 0])
   _plt.scatter(p_W_frames[:, 2], -p_W_frames[:, 0])
 
-#no_of_frames = 4
-#res = run_ba(cropped_hidden_states, cropped_observations, K)
-#plot_map(plt, res, cropped_observations)
-#plt.xlim([0, 20])
-#plt.ylim([-5, 5])
 
-no_of_frames = NUM_FRAMES
-res = run_ba(hidden_states, observations, K)
-plot_map(plt, hidden_states, observations)
-plt.xlim([0, 40])
-plt.ylim([-10, 10])
+def run_ba(hstates, obsers, kmat):
+  """
+  Run BA on image points and actual positions in 3D
+  @param: hstates: image coordinates of points
+  @param: aobsers: actual 3D point in the world
+  @param: kmat: Intrinsics of the camera
+  """
+  jacob = calc_jacobian(hstates, obsers)
+  x0 = hstates
+  # err = error_function(hstates, obsers, kmat)
+  res = optimize.least_squares(error_function, x0,
+                               x_scale='jac', method="trf",
+                               jac_sparsity=jacob, verbose=2,
+                               max_nfev=100,
+                               args=(obsers, kmat))
+  return res.x
 
-#T_V_C = res[: NUM_FRAMES * 6].reshape(-1, 6)
-#p_V_C = np.zeros((NUM_FRAMES, 3))
-#
-#for i in range(NUM_FRAMES):
-#  single_T_V_C = hp.twist_to_homo_mat(T_V_C[i])
-#  p_V_C[i, :] = single_T_V_C[:3, 3]
-#
-#p_G_C = align_estimate_to_gt(pp_G_C, p_V_C)
-#plt.plot(pp_G_C[:, 2], -pp_G_C[:, 0], color='g', label='Ground Truth')
-#plt.plot(p_V_C[:, 2], -p_V_C[:, 0], color='r', label='Original estimate')
-#plt.plot(p_G_C[:, 2], -p_G_C[:, 0], color='b', label='Aligned estimate')
+if __name__ == "__main__":
+  hidden_states = np.loadtxt("../data/hidden_state.txt")
+  observations = np.loadtxt("../data/observations.txt")
+  NUM_FRAMES = 150
+  K = np.loadtxt("../data/K.txt")
+  poses = np.loadtxt("../data/poses.txt")
+  pp_G_C = poses[:, [3, 7, 11]]
 
-plt.legend()
-plt.show()
+  test_no_frame = 8
+  hidden_states, observations, pp_G_C = hp.crop_problem(hidden_states,
+                                                        observations,
+                                                        pp_G_C,
+                                                        NUM_FRAMES)
+  cropped_hidden_states, cropped_observations, _ = hp.crop_problem(hidden_states,
+                                                                   observations,
+                                                                   pp_G_C,
+                                                                   test_no_frame)
 
+  T_V_C = hidden_states[: NUM_FRAMES * 6].reshape(-1, 6)
+  p_V_C = np.zeros((NUM_FRAMES, 3))
+
+  for i in range(NUM_FRAMES):
+    single_T_V_C = hp.twist_to_homo_mat(T_V_C[i])
+    p_V_C[i, :] = single_T_V_C[:3, 3]
+
+  p_G_C = align_estimate_to_gt(pp_G_C, p_V_C)
+
+  #plt.plot(pp_G_C[:, 2], -pp_G_C[:, 0], color='g', label='Ground Truth')
+  #plt.plot(p_V_C[:, 2], -p_V_C[:, 0], color='r', label='Original estimate')
+  #plt.plot(p_G_C[:, 2], -p_G_C[:, 0], color='b', label='Aligned estimate')
+
+  #plt.legend()
+  #plt.show()
+
+  #res = run_ba(cropped_hidden_states, cropped_observations, K)
+  #plot_map(plt, res, cropped_observations)
+  #plt.xlim([0, 20])
+  #plt.ylim([-5, 5])
+  
+  no_of_frames = NUM_FRAMES
+  no_of_frames = test_no_frame
+  res = run_ba(hidden_states, observations, K)
+  
+  T_V_C = res[: NUM_FRAMES * 6].reshape(-1, 6)
+  p_V_C = np.zeros((NUM_FRAMES, 3))
+
+  for i in range(NUM_FRAMES):
+    single_T_V_C = hp.twist_to_homo_mat(T_V_C[i])
+    p_V_C[i, :] = single_T_V_C[:3, 3]
+
+  opt_p_G_C = align_estimate_to_gt(pp_G_C, p_V_C)
+  plt.plot(pp_G_C[:, 2], -pp_G_C[:, 0], color='g', label='Ground Truth')
+  plt.plot(p_G_C[:, 2], -p_G_C[:, 0], color='r', label='Aligned estimate')
+  plt.plot(opt_p_G_C[:, 2], -opt_p_G_C[:, 0], color='b', label='Opt estimate')
+
+  plt.legend()
+  plt.show()
